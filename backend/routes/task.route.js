0adc0c03d23ea65fdf035a4bc68b7d39058a19e5
Epraldo88 <eko.prasetyo1314@gmail.com>
2025-10-16 13:08:28 +0700

    add task routing

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { getTask } from "../controllers/task.controller.js";
+import {
+  createTask,
+  deleteTask,
+  getTask,
+  updateTask,
+} from "../controllers/task.controller.js";
 
 const router = express.Router();
 
 router.get("/", getTask);
+router.post("/", createTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 export default router;
